fcf943a28297613dcd306adbf6a9c07e5f757bb8
hiro22022 <hiro22022@gmail.com>
2017-08-20 23:06:47 +0900

    2017.8.20 temporary update

--- TECS-First/texnize.rb
+++ TECS-First/texnize.rb
@@ -218,7 +218,7 @@ bookmarkstype=toc]{hyperref}
 %\\usepackage[bookmarksnumbered=true,dvipdfm]{hyperref}
 
 % プリアンブル
-\\title{組込みコンポーネントシステム TECS \\ 初めの一歩}
+\\title{組込みコンポーネントシステム TECS \\\\ 初めの一歩}
 \\author{大山 博司} 
 \\date{2017年8月15日}
 
